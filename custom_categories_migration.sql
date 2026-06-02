1cae042eaf8d61feced968595e52e44ef02ebff8
bbalaji06 <bhupatibalajinaidu@gmail.com>
2026-06-02 17:35:47 +0530

    updated

--- custom_categories_migration.sql
+++
@@ -1,5 +0,0 @@
--- Migration to add custom_categories to the users table
--- Adds a text array column to store user-defined task categories, pre-populated with defaults.
-
-ALTER TABLE public.users 
-ADD COLUMN IF NOT EXISTS custom_categories text[] NOT NULL DEFAULT '{Work, Meeting, Call, Personal, Review, Break, Health}'::text[];
